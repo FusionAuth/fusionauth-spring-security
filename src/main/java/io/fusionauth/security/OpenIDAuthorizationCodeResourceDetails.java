464b639595c371a92780ecf1fca094dc0c2434ba
Daniel DeGroff <daniel@inversoft.com>
2018-10-23 10:08:20 -0600

    JavaDoc

--- src/main/java/io/fusionauth/security/OpenIDAuthorizationCodeResourceDetails.java
+++ src/main/java/io/fusionauth/security/OpenIDAuthorizationCodeResourceDetails.java
@@ -3,9 +3,11 @@ package io.fusionauth.security;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 
 /**
- * Add some OpenID stuff.
+ * Extend the {@link AuthorizationCodeResourceDetails} to add the OpenID Connect UserInfo endpoint as described
+ * in section <code>5.3</code> of OpenID Connect Core 1.0.
  *
  * @author Tyler Scott
+ * @see <a href="https://openid.net/specs/openid-connect-core-1_0.html#UserInfo">https://openid.net/specs/openid-connect-core-1_0.html#UserInfo</a>
  */
 public class OpenIDAuthorizationCodeResourceDetails extends AuthorizationCodeResourceDetails {
   private String userInfoUri;
